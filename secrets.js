9cbf0c77ce031b503395d25ff933f966fd43abb7
prathamchande <prathamchande@gmail.com>
2023-04-03 04:41:22 +0530

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,5 +1,16 @@
 // secrets.js
 
-const apiKey = process.env.MY_SECRET_NAME;
+ const apiKey = process.env.MY_SECRET_NAME;
 
 export { apiKey };
+
+
+function sayHi(user) {
+  alert(`Hello, ${user}!`);
+}
+
+function sayBye(user) {
+  alert(`Bye, ${user}!`);
+}
+
+export {sayHi, sayBye}; /
