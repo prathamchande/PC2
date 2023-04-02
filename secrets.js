ec277758e603bc2a6a330bf4087534bdc2b3fbbf
prathamchande <prathamchande@gmail.com>
2023-04-03 05:22:01 +0530

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,12 +1,13 @@
-// secrets.js
+const API_KEY = process.env.MY_SECRET_NAME;
 
+fetch(`https://ap-south-1.aws.data.mongodb-api.com/app/prathamapi1-vnxon/endpoint/triviaQA?secret=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+  setAllDataKey = data;
 
-function sayHi(user) {
-  alert(`Hello, ${user}!`);
-}
-
-function sayBye(user) {
-  alert(`Bye, ${user}!`);
-}
-
-export * as {sayHi, sayBye}; 
+          console.log("apik", setAllDataKey);
+  })
+  .catch(error => {
+   
+          console.log("apinotwork");
+  });
