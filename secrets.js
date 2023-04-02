2996573c8de8406d27965175388ced3a06af8e29
prathamchande <prathamchande@gmail.com>
2023-04-03 05:24:05 +0530

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -2,6 +2,7 @@ const API_KEY = process.env.MY_SECRET_NAME;
   onload();
 
     function onload() {
+         console.log("API_KEY", API_KEY);
 fetch(`https://ap-south-1.aws.data.mongodb-api.com/app/prathamapi1-vnxon/endpoint/triviaQA?secret=${API_KEY}`)
   .then(response => response.json())
   .then(data => {
